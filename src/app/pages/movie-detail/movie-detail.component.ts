359e83ce0f7d79dc57daf2b80293e44eb3677cd2
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2018-06-02 23:51:59 -0300

    - Adicionado timer em Movie Detail

--- src/app/pages/movie-detail/movie-detail.component.ts
+++ src/app/pages/movie-detail/movie-detail.component.ts
@@ -20,6 +20,12 @@ export class MovieDetailComponent implements OnInit {
 
   movie: Movie;
 
+  // Countdown Timer
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+
   private imgUrl: string = 'https://image.tmdb.org/t/p/w780/';
 
   constructor(
@@ -50,10 +56,45 @@ export class MovieDetailComponent implements OnInit {
           this.movie = res;
           this.breadcrumbService.addFriendlyNameForRouteRegex('/movie-detail/[0-9]', this.movie.title);
           this.titleService.setTitle(this.movie.title || this.movie.original_title);
+          this.countdownTimer();
         },
         error => {
           console.log(error);
         });
+
+  }
+
+  /**
+   * Functions
+   */
+  countdownTimer(): void {
+
+    let x = setInterval(() => {
+
+      // Get todays date and time
+      let now = new Date().getTime();
+      let countDownDate = new Date(this.movie.release_date);
+
+      // Find the distance between now an the count down date
+      let distance = Number(countDownDate) - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // If the count down is finished.
+      if (distance < 0) {
+        clearInterval(x);
+        this.days = 0;
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+      }
+
+    }, 1000);
+
 
   }
 
